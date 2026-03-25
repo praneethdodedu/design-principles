5726bc962281a30e657c6b7310bcac3578b4279f
Praneeth Dodedu <praneeth.dodedu@ascendlearning.com>
2026-03-25 15:54:07 +0530

    Implement Dependency Inversion Principle with Relationships and Person classes

---
+++ design-patterns/dependency_inversion_principle.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+class Relationship(Enum):
+    PARENT = 1
+    CHILD = 2
+    SIBLING = 3
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+class RelationshipBrowser:
+    def find_all_children_of(self, name):
+        pass
+
+class Relationships(RelationshipBrowser):
+    def __init__(self):
+        self.relations = []
+
+    def add_parent_and_child(self, parent, child):
+        self.relations.append((parent, Relationship.PARENT, child))
+        self.relations.append((child, Relationship.CHILD, parent))
+    
+    def find_all_children_of(self, name):
+        for r in self.relations:
+            if r[0].name == name and r[1] == Relationship.PARENT:
+                yield r[2].name
+
+class Research:
+    def __init__(self, browser):
+        for child in browser.find_all_children_of("John"):
+            print(f"John has a child named {child}")
+        
+    
+if __name__ == "__main__":
+    parent = Person("John")
+    child1 = Person("Chris")
+    child2 = Person("Matt")
+
+    relationships = Relationships()
+    relationships.add_parent_and_child(parent, child1)
+    relationships.add_parent_and_child(parent, child2)
+
+    Research(relationships)
+    
+        
